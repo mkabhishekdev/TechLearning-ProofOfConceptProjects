e494f6c23639d228fb027d18b4a884ab7ccd54f1
coderkemp <axm3737@g.rit.edu>
2019-07-24 18:47:33 -0400

    stack queue problems

--- GeeksForGeeks/src/queue/QueueUsingArrayImpl.java
+++ GeeksForGeeks/src/queue/QueueUsingArrayImpl.java
@@ -1,10 +1,36 @@
 package queue;
 
 public class QueueUsingArrayImpl {
-
+	int arr[];
+	int index=0;
+	int size;
+	
+	public void push(int data) {
+		arr[index]=data;
+		index++;
+	}
+	
+	public int pop() {
+	   int toDelete = arr[0];
+	   for(int i=0;i<arr.length;i++) {
+		   i=i+1;
+	   }
+	}
+	
+	public QueueUsingArrayImpl(int size) {
+	        this.size = size;
+	        arr = new int[size];
+	    } 
+	   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+         QueueUsingArrayImpl queue = new QueueUsingArrayImpl(6);
+         queue.push(1);     
+         queue.push(2);    
+         queue.push(3);    
+         queue.push(4);    
+         queue.push(5);    
+         queue.push(6);    
 	}
 
 }
--- GeeksForGeeks/src/stack/StackUsingArrayImpl.java
+++ GeeksForGeeks/src/stack/StackUsingArrayImpl.java
@@ -1,10 +1,57 @@
 package stack;
 
-public class StackUsingArrayImpl {
 
+public class StackUsingArrayImpl {
+    int arr[];
+    int size;
+    int index=0;
+    
+    public StackUsingArrayImpl(int size) {
+        this.size = size;
+        arr = new int[size];
+    } 
+    
+	public void push(int data) {
+	   arr[index]=data;
+	   index++;
+	}
+	
+	public int pop() {
+	        return arr[--index];
+	}
+	
+	public boolean isEmpty() {
+		if(arr.length==0)return true;
+		return false;
+	}
+	
+	public int size() {
+		size = arr.length;
+		return size;
+	}
+	
+	public boolean isFull() {
+		if(index==size-1)return true;
+		else return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+            StackUsingArrayImpl stack = new StackUsingArrayImpl(5);
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+            stack.push(4);
+            stack.push(5);
+            stack.pop();
+            System.out.println("1. Size of stack after push operations: " + stack.size());
 
+            System.out.printf("2. Pop elements from stack : ");
+            while (!stack.isEmpty()) {
+                System.out.printf(" %d", stack.pop());
+            }
+
+            System.out.println("\n3. Size of stack after pop operations : " + stack.size());
 	}
 
 }
