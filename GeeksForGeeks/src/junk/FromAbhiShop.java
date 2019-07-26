f9969fbc7bf68ffd3bf470ddaf5baf2e4785c188
abhi <axm3737@rit.edu>
2019-07-25 22:23:45 -0400

    queue work

---
+++ GeeksForGeeks/src/junk/FromAbhiShop.java
@@ -0,0 +1,22 @@
+package junk;
+
+public class FromAbhiShop {
+
+}
+/*
+
+ <%
+System.out.println("Username: "+username);
+System.out.println("ProductID: "+resultset.getString(2));
+ ResultSet retrieveUserID = statement.executeQuery("select userid from registration where username=\""+username+"\"");
+ System.out.println(retrieveUserID.getInt(1));
+ int userID = retrieveUserID.getInt(1);
+ ResultSet retrieveProductID = statement.executeQuery("select productid from inventory where product=\""+resultset.getString(2)+"\"");
+ String productID = retrieveProductID.getString(1);
+ request.setAttribute("userid",userID);
+ request.setAttribute("productid",productID);
+ request.setAttribute("price",resultset.getString(3));
+ request.setAttribute("productname",resultset.getString(2));
+%> 
+
+*/
--- GeeksForGeeks/src/queue/QueueUsingArrayImpl.java
+++ GeeksForGeeks/src/queue/QueueUsingArrayImpl.java
@@ -1,36 +1,42 @@
 package queue;
 
 public class QueueUsingArrayImpl {
-	int arr[];
-	int index=0;
-	int size;
+    int[] arr;
+    int index=0;
+    int front,rear;
+    int size;
 	
-	public void push(int data) {
-		arr[index]=data;
-		index++;
+	public void enqueue(int data) {
+     arr[index]=data;
+     index++;
 	}
 	
-	public int pop() {
-	   int toDelete = arr[0];
-	   for(int i=0;i<arr.length;i++) {
-		   i=i+1;
-	   }
-	}
+	public int dequeue() {
+	  size=arr.length;
+	  int temp=arr[0];
+	  for(int i=0;i<arr.length-1;i++){
+		  arr[i]=arr[i+1];
+	  }
+	  size=size-1;
+	//  arr[size]=arr[size-1];
+	//  arr = arr[size];
+	  return temp;
+	} 
 	
 	public QueueUsingArrayImpl(int size) {
-	        this.size = size;
 	        arr = new int[size];
 	    } 
 	   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          QueueUsingArrayImpl queue = new QueueUsingArrayImpl(6);
-         queue.push(1);     
-         queue.push(2);    
-         queue.push(3);    
-         queue.push(4);    
-         queue.push(5);    
-         queue.push(6);    
+         queue.enqueue(1);
+         queue.enqueue(2);
+         queue.enqueue(3);
+         queue.enqueue(4);
+         queue.enqueue(5);
+         queue.enqueue(6);
+         queue.dequeue();
 	}
 
 }
