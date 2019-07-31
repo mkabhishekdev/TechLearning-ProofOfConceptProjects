41e5c19abcae9d50aeb5c377fbdeed0ffe81632b
coderkemp <axm3737@g.rit.edu>
2019-07-30 21:36:00 -0400

    tree work

---
+++ GeeksForGeeks/src/trees/Node.java
@@ -0,0 +1,12 @@
+package trees;
+
+class Node{
+	int data;
+	Node left;
+	Node right;
+	Node(int x){
+		x=data;
+	}
+}
+
+
---
+++ GeeksForGeeks/src/trees/TreeOperate.java
@@ -0,0 +1,12 @@
+package trees;
+
+public class TreeOperate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Node root = new Node(10);
+        root.left= new Node(5);
+        root.right = new Node(3);
+	}
+
+}
