c88bf9527036ac5b04c5cbd0dd37151f9be52b3c
coderkemp <axm3737@g.rit.edu>
2019-08-01 21:19:17 -0400

    tree work

--- GeeksForGeeks/src/trees/LevelOrder.java
+++ GeeksForGeeks/src/trees/LevelOrder.java
@@ -1,10 +1,44 @@
 package trees;
-
+import java.util.*;
+/*ALGORITHM
+ 
+ */
 public class LevelOrder {
+    Node root;
+    
+    public List<Node> levelTraversal(){
+    	if(root == null) {
+    		return null;
+    	}
+    	List<Node> res = new ArrayList<Node>();
+    	Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        Node curr=root;
+        while(!queue.isEmpty()) {
+        	curr=queue.poll();
+        	res.add(curr);
+        	if(curr.left!=null) {
+        		queue.add(curr.left);
+        	
+        	}
+        	if(curr.right!=null) {
+        		queue.add(curr.right);
 
+        	}
+        }
+        return res;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       LevelOrder lo = new LevelOrder();
+       lo.root = new Node(10);
+       lo.root.left = new Node(3);
+       lo.root.right = new Node(8);
+       lo.root.left.left = new Node(9);
+       lo.root.left.right = new Node(12);
+       lo.root.right.left = new Node(7);
+       System.out.println(Arrays.toString(lo.levelTraversal().toArray()));
 	}
 
 }
--- GeeksForGeeks/src/trees/MaxWidth.java
+++ GeeksForGeeks/src/trees/MaxWidth.java
@@ -1,6 +1,6 @@
- package trees;
+package trees;
 
-public class PostOrder {
+public class MaxWidth {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
