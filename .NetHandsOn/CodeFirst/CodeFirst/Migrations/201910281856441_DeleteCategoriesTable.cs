485b3b767600f74427290f48c7a6503f5c4d6ba9
Kempanna <abhi.kempanna@tylertech.com>
2019-10-31 11:46:43 -0400

    adding projects to .net hands on folder

---
+++ .NetHandsOn/CodeFirst/CodeFirst/Migrations/201910281856441_DeleteCategoriesTable.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Categories");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
---
+++ .NetHandsOn/SimpleHeartbeatService/SimpleHeartbeatService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SimpleHeartbeatService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Heartbeat>(s =>
+                {
+                    s.ConstructUsing(heartbeat => new Heartbeat());
+                    s.WhenStarted(heartbeat => heartbeat.Start());
+                    s.WhenStopped(heartbeat => heartbeat.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("HeartBeatService");
+                x.SetDisplayName("HeartBeat Service");
+                x.SetDescription("this is the sample service used in a demo");
+            }
+            );
+            int exitCodeValue = (int)Convert.ChangeType(exitCode,exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+
+        }
+    }
+}
