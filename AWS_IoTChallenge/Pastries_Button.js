14d2cb1f947b25863947e403ee09593ecc647227
Kempanna <abhi.kempanna@tylertech.com>
2020-02-21 16:04:08 -0500

    iot work

--- AWS_IoTChallenge/Pastries_Button.js
+++ AWS_IoTChallenge/Pastries_Button.js
@@ -13,14 +13,14 @@ const AWS = require('aws-sdk');
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 
 // !!CHANGE THIS!! Enter the ARN of your SNS Topic
-const TOPIC_ARN = 'arn:aws:sns:us-east-1:282052529943:DevTreats';
+const TOPIC_ARN = 'arn:aws:sns:us-east-1:282052529943:DevTreats';   //my SNS topic I created
 
 exports.handler = (event, context, callback) => {
 
 // !!CHANGE THIS!! Your text to display goes in the single quotation marks below    
-    const singleClick = 'We have a Birthday today! Donuts on the Usual place!!';
-    const doubleClick = 'We have a Birthday today! Pastries on the Usual place!!';
-    const longClick = 'Come to the Plaza for a treat surprise now!!';
+    const singleClick = 'We have a Birthday today! Donuts on the Usual place!!';  // Birthday mail, I know surprise lies in finding who's birthday
+    const doubleClick = 'No special reason! Pastries on the Usual place!!';  // Generic email for pastries
+    const longClick = 'Immediately please assemble in the Plaza!!';  // In the case of any emergencies
    
 var devTeamAlert = singleClick;   
 
@@ -41,3 +41,7 @@ if(event.clickType == "LONG"){
         // result will go to function callback
         SNS.publish(params, callback);
 };
+
+//email is: VST-Development@tylertech.com
+// create the dynamo db feature for data analysis
+
