d92063b90dd463aec64cce9fa30e4c95a33b25d1
Kempanna <abhi.kempanna@tylertech.com>
2020-03-02 15:47:35 -0500

    iot work

--- AWS_IoTChallenge/Pastries_Button.js
+++ AWS_IoTChallenge/Pastries_Button.js
@@ -12,15 +12,18 @@
 const AWS = require('aws-sdk');
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 
-// !!CHANGE THIS!! Enter the ARN of your SNS Topic
 const TOPIC_ARN = 'arn:aws:sns:us-east-1:282052529943:DevTreats';   //my SNS topic I created
+
+//creation if DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion:'2010-03-31'});
+
 
 exports.handler = (event, context, callback) => {
 
 // !!CHANGE THIS!! Your text to display goes in the single quotation marks below    
     const singleClick = 'We have a Birthday today! Donuts on the Usual place!!';  // Birthday mail, I know surprise lies in finding who's birthday
     const doubleClick = 'No special reason! Pastries on the Usual place!!';  // Generic email for pastries
-    const longClick = 'Immediately please assemble in the Plaza!!';  // In the case of any emergencies/to assemble everyone in a common place
+    const longClick = 'Announcement! please assemble in the Usual place!!';  // In the case of any emergencies/to assemble everyone in a common place
    
 var devTeamAlert = singleClick;   
 
@@ -31,10 +34,27 @@ if(event.clickType == "DOUBLE"){
 if(event.clickType == "LONG"){
     devTeamAlert = longClick;
 }
-    
+
+// want to store any dev message triggered in the DevTreats table in DynamoDB
+var storeInDb = {
+    TableName: 'DevTreatsTable',
+    Item: {
+        'message' : {S : devTeamAlert}
+    }
+};
+
+// call to db to add the item to DevTreats table
+ddb.putItem(storeInDb, function(err,data){
+  if(err){
+      console.log("Error",err);
+  }
+  else{
+      console.log("Success",data);
+  }
+});
+
  const params = {
             Message: devTeamAlert,
-            // !!CHANGE THIS!! Enter the subject of your email in the single quotation marks below
             Subject: `Treats!`,
             TopicArn: TOPIC_ARN,
         };
