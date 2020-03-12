9bc34ef38856f051fdf4b1783e0989c9d0cab73e
Kempanna <abhi.kempanna@tylertech.com>
2020-03-12 16:49:42 -0400

    iot work

--- AWS_IoTChallenge/Pastries_Button.js
+++ AWS_IoTChallenge/Pastries_Button.js
@@ -4,9 +4,8 @@
  * This is a Lambda function that sends an Email on the click of an AWS IoT
  * button by publishing to an SNS topic.
  *
- * 1. Enter the ARN (Amazon Resource Name) of the SNS topic you have created.
- * 2. Edit the content of the message to be delivered upon single clicks, double clicks, and 
- * long clicks by changing the text within the single quotation marks
+ * 1. arn:aws:sns:us-east-1:282052529943:DevTreats is the SNS topic created.
+
  */
 
 const AWS = require('aws-sdk');
@@ -14,13 +13,12 @@ const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 
 const TOPIC_ARN = 'arn:aws:sns:us-east-1:282052529943:DevTreats';   //my SNS topic I created
 
-//creation if DynamoDB service object
+//creation of DynamoDB service object
 var ddb = new AWS.DynamoDB({apiVersion:'2010-03-31'});
 
 
 exports.handler = (event, context, callback) => {
-
-// !!CHANGE THIS!! Your text to display goes in the single quotation marks below    
+    
     const singleClick = 'We have a Birthday today! Donuts on the Usual place!!';  // Birthday mail, I know surprise lies in finding who's birthday
     const doubleClick = 'No special reason! Pastries on the Usual place!!';  // Generic email for pastries
     const longClick = 'Announcement! please assemble in the Usual place!!';  // In the case of any emergencies/to assemble everyone in a common place
@@ -35,7 +33,7 @@ if(event.clickType == "LONG"){
     devTeamAlert = longClick;
 }
 
-// want to store any dev message triggered in the DevTreats table in DynamoDB
+// want to store any dev message triggered in the DevTreats table in DynamoDB table named 'DevTreatsTable'
 var storeInDb = {
     TableName: 'DevTreatsTable',
     Item: {
@@ -44,12 +42,12 @@ var storeInDb = {
 };
 
 // call to db to add the item to DevTreats table
-ddb.putItem(storeInDb, function(err,data){
+ddb.putItem(storeInDb, function(err,devTeamAlert){
   if(err){
       console.log("Error",err);
   }
   else{
-      console.log("Success",data);
+      console.log("Success",devTeamAlert);
   }
 });
 
@@ -63,5 +61,4 @@ ddb.putItem(storeInDb, function(err,data){
 };
 
 //email is: VST-Development@tylertech.com
-// create the dynamo db feature for data analysis
 //Device: G030PM034053VTT3
