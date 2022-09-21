8951c39662307c0b215e5eda0933ba3c27fba764
Abhi Kempanna <32283903+coderkemp@users.noreply.github.com>
2022-09-21 16:33:27 -0400

    MAUI Blazor concepts

---
+++ .NetHandsOn/.NET_MAUIBlazor(Multi Application User Interface)/BlazorMauiApp/RazorLibrary/Models/TodoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorLibrary.Models;
+
+public class TodoModel
+{
+
+    public string TodoItem { get; set; }
+    public bool IsComplete { get; set; }
+
+
+}
---
+++ .NetHandsOn/ApiDemoApp/ApiDemo/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDemo.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [MinLength(length:10)]
+        public string FirstName { get; set; }
+    }
+
+
+}
